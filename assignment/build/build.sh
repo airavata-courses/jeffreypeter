269b3f729c0dd8bd3bb34ad1c33f1c31a7161f3d
jeffravi <jeffravi@iu.edu>
2017-09-17 00:19:19 -0400

    fisnished build.sh

--- assignment/build/build.sh
+++ assignment/build/build.sh
@@ -5,8 +5,9 @@ echo IN::$PWD;
 docker rm -f $(docker ps -a -q) || echo No Docker images
 # Delete all images
 docker rmi -f $(docker images -q) || echo No Docker containers
-docker pull jeffrav/gateway;docker pull jeffrav/nodejs:latest;docker pull jeffrav/python:latest;docker pull jeffrav/java:latest;
-docker run -d --name gateway -p 8000:8000 -it jeffrav/gateway;
+docker pull jeffrav/ui;docker pull jeffrav/gateway;docker pull jeffrav/nodejs:latest;docker pull jeffrav/python:latest;docker pull jeffrav/java:latest;
+docker run -d --name ui -p 7000:7000 jeffrav/ui;
+docker run -d --name gateway -p 8000:8000 jeffrav/gateway;
 docker run -d --name nodejs jeffrav/nodejs;
 docker run -d --name python jeffrav/python;
 docker run -d --name java jeffrav/java;
